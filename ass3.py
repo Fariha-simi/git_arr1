ef41b4457d4055a5ffaeea3c4d1f4799ff445906
Fariha-simi <farihatasnimsimi.physics@gmail.com>
2020-10-09 10:40:13 -0700

    2nd commit

--- ass3.py
+++ ass3.py
@@ -18,3 +18,13 @@ print(arr)
 
 arr2 = arr[3][2].pop(2)
 print(arr)
+
+arr[3][2].append("string5")
+print(arr)
+
+arr3 = [3, 5, 1, 3, 5, 4, 10]
+arr3.sort()
+print(arr3)
+
+arr3.reverse()
+print(arr3)
