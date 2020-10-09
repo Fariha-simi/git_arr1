25584d7828b72f5c1d1fa2c14e51574c28b097d8
Fariha-simi <farihatasnimsimi.physics@gmail.com>
2020-10-09 10:57:09 -0700

    2nd commit

--- ass1.py
+++ ass1.py
@@ -1,13 +1,20 @@
-arr = [1, 2, 3, 10, 11, 16, 18, 4, 43, 2, 2, 4]
+arr = [1, 2, 3, 16, 18, 4, 43, 2, 2, 4]
 
 threshold1 = 2
 threshold2 = 4
 
-for element in arr:
+twoexist = False
+fourexist = False
+
+for element in arr:   
+    
     if element == threshold1 :
-        print("True")
-    elif element == threshold2 :
-        print("True")        
+        twoexist = True
+        print("2 exists")
+        
+    if element == threshold2 :
+        fourexist = True
+        print("4 exists")
 
     else:
         print("false")
