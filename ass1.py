adf5e0f06a88368df3c6eff8813d25c156db6f20
Fariha-simi <farihatasnimsimi.physics@gmail.com>
2020-10-09 09:39:23 -0700

    first commit

---
+++ ass1.py
@@ -0,0 +1,9 @@
+arr = [1, 2, 3, 10, 11, 16, 18, 4, 43, 2, 2, 4]
+threshold1 = 2
+threshold2 = 4
+for element in arr:
+    if element == threshold1 and element==threshold2:
+        print(True)
+
+    else:
+        print(False)
